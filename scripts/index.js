b67c13828507535be1f7c66e5850d2848258dd2c
Nu11Try <nu11try@gmail.com>
2019-01-03 14:26:35 +0300

    Блок с преимуществами

--- scripts/index.js
+++ scripts/index.js
@@ -17,12 +17,11 @@ $(function(){
             });
         },
         function() {
-            console.log("OUT");
             $(".element_hover[id="+idElement+"]").css({
                 "display": "none"
             });
             $(".partners_element[id="+idElement+"]").css({
-                "border": "solid 1px rgb(0, 0, 0)"
+                "border": "solid 1px rgb(229, 229, 229)"
             });
             $(".partners_element[id="+idElement+"] .element_image").css({
                 "filter": "blur(0px)"
